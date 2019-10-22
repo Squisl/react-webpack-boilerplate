ca55e1eac1a26a57196af39482d00303c433e304
liew <joeyliew@tutanota.com>
2019-10-23 00:47:11 +0200

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.module.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <span>App</span>
+    </div>
+  );
+};
+
+export default App;
